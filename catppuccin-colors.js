5585693e816fdbc449db02a8644cb05b1b7ee397
Charlize de Beer <charlizedb@aie.ac>
2024-12-18 13:16:06 +0200

    Colour Palette and Shadows

---
+++ catppuccin-colors.js
@@ -0,0 +1,21 @@
+// catppuccin-colors.js
+module.exports = [
+    { name: 'Rosewater', hex: '#f2d5cf', rgb: 'rgb(242, 213, 207)' },
+    { name: 'Flamingo', hex: '#eebebe', rgb: 'rgb(238, 190, 190)' },
+    { name: 'Pink', hex: '#f4b8e4', rgb: 'rgb(244, 184, 228)' },
+    { name: 'Mauve', hex: '#ca9ee6', rgb: 'rgb(202, 158, 230)' },
+    { name: 'Red', hex: '#e78284', rgb: 'rgb(231, 130, 132)' },
+    { name: 'Maroon', hex: '#ea999c', rgb: 'rgb(234, 153, 156)' },
+    { name: 'Peach', hex: '#ef9f76', rgb: 'rgb(239, 159, 118)' },
+    { name: 'Yellow', hex: '#e5c890', rgb: 'rgb(229, 200, 144)' },
+    { name: 'Green', hex: '#a6d189', rgb: 'rgb(166, 209, 137)' },
+    { name: 'Teal', hex: '#81c8be', rgb: 'rgb(129, 200, 190)' },
+    { name: 'Sky', hex: '#99d1db', rgb: 'rgb(153, 209, 219)' },
+    { name: 'Sapphire', hex: '#85c1dc', rgb: 'rgb(133, 193, 220)' },
+    { name: 'Blue', hex: '#8caaee', rgb: 'rgb(140, 170, 238)' },
+    { name: 'Lavender', hex: '#babbf1', rgb: 'rgb(186, 187, 241)' },
+    { name: 'Surface', hex: '#626880', rgb: 'rgb(98, 104, 128)' },
+    { name: 'Base', hex: '#303446', rgb: 'rgb(48, 52, 70)' },
+    { name: 'Mantle', hex: '#292c3c', rgb: 'rgb(41, 44, 60)' },
+  ];
+  
--- sketch-skew.js
+++ sketch-skew.js
@@ -1,18 +1,30 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const catppuccinColors = require('./catppuccin-colors');
+const Color = require('canvas-sketch-util/color');
+const risoColors = require('riso-colors');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
 const sketch = ({context, width, height}) => {
-  let x, y, w, h;
+  let x, y, w, h, fill, stroke;
+  let shadowColor;
 
   const num = 20;
   const degrees = -30;
 
   const rects = []
+
+  const rectColors = [
+    random.pick(catppuccinColors),
+    random.pick(catppuccinColors),
+    random.pick(catppuccinColors)
+  ]
+
+  const bgColor = random.pick(catppuccinColors).hex
 
   for (let i = 0; i < num; i++){
     x = random.range( 0, width);
@@ -20,21 +32,42 @@ const sketch = ({context, width, height}) => {
     w = random.range( 200, 600);
     h = random.range( 40, 200);
 
-    rects.push({x, y, w, h});
+   fill = random.pick(rectColors).hex;
+  //fill =  `rgba(${random.range(133, 242)} , ${random.range(158, 213)}, ${random.range(207, 230)}, 1)`;
+   stroke = random.pick(rectColors).hex;
+  // stroke = "#292c3c";
+
+    rects.push({x, y, w, h, fill, stroke});
   }
 
   return ({ context, width, height }) => {
-    context.fillStyle = '#292c3c';
+    context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
 
     rects.forEach(rect => { 
-    const {x, y, w , h} = rect;
+    const {x, y, w , h, fill, stroke} = rect;
 
     context.save();
     context.translate(x , y);
-    context.strokeStyle = '#f4b8e4';
+
+    context.strokeStyle = stroke;
+    context.fillStyle = fill;
+    context.lineWidth = 15;
+    
 
     drawSkewedRect({context, w, h, degrees})
+
+    shadowColor = Color.offsetHSL(fill, 0, 0, -20)
+    shadowColor.rgba[3] = 0.5;
+
+    context.shadowColor = Color.style(shadowColor.rgba);
+    context.shadowOffsetX = -10;
+    context.shadowOffsetY = 20;
+
+    context.fill();
+
+    context.shadowColor = null
+
     context.stroke();
 
    context.restore();
