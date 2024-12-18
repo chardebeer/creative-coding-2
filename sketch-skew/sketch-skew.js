0db24eae97c6a9be525c430f2adbc08c59f8df95
Charlize de Beer <charlizedb@aie.ac>
2024-12-18 15:09:05 +0200

    Restructure files

--- sketch-skew/sketch-skew.js
+++ sketch-skew/sketch-skew.js
@@ -1,7 +1,7 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
-const catppuccinColors = require('./catppuccin-colors');
+const catppuccinColors = require('../catppuccin-colors');
 const Color = require('canvas-sketch-util/color');
 const risoColors = require('riso-colors');
 
